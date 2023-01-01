91cd20cc3ff728f3aa1768da867da2176c1bbd84
Wil <william.neeley@gmail.com>
2023-01-01 08:52:11 -0900

    Create http.services.ts

---
+++ src/http/http.services.ts
@@ -0,0 +1,39 @@
+import fetch from 'cross-fetch'
+
+export class HttpServices {
+
+    async getAsync(url: string, options: any = {}): Promise<Response> {
+        return await fetch(url, {
+            method: 'GET',
+            ...options
+        })
+    }
+
+    async postAsync(url: string, options: any = {}): Promise<Response> {
+        return await fetch(url, {
+            method: 'POST',
+            ...options
+        })
+    }
+
+    async putAsync(url: string, options: any = {}): Promise<Response> {
+        return await fetch(url, {
+            method: 'PUT',
+            ...options
+        })
+    }
+
+    async patchAsync(url: string, options: any = {}): Promise<Response> {
+        return await fetch(url, {
+            method: 'PATCH',
+            ...options
+        })
+    }
+
+    async deleteAsync(url: string, options: any = {}): Promise<Response> {
+        return await fetch(url, {
+            method: 'DELETE',
+            ...options
+        })
+    }
+}
